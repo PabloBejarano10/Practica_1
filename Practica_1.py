60fd43aaaa9f7dacb9895c3c13f05cd17ae77580
PabloBejarano10 <manbejar@ucm.es>
2023-03-02 10:35:20 +0100

    Terminado sin cambiar el resul de lista a array

--- Practica_1.py
+++ Practica_1.py
@@ -5,9 +5,8 @@ from multiprocessing import Value, Array
 from time import sleep
 from random import random, randint
 
-
 NPROD = 3
-N = 10 #cantidad de numeros generados por cada productor 
+N = 5 #cantidad de numeros generados por cada productor 
 
 def delay(factor = 3):
     sleep(random()/factor)
@@ -17,40 +16,108 @@ def add_number(storage, index, data, full):
     storage[index.value] = data
     delay(6)
     index.value = index.value + 1
+    
+    
+def index_min(a):
+    
+    l = []
+    for x in a:
+        l.append(x.value)
+    m = minimo(a)
+    indice = l.index(m)
+    
+    return indice
 
-def gen_number(number):
-    """
-    Actualiza el valor de number 
-    """
-    number.Value += randint(1,3)
-    
-def producer(sem, number, full):
-    sem.acquire()
-    gen_number(number)
-    full.release()
-    
-def index_min(a,minimo):
-     if minimo in a:   
-        for i in range(len(a)):
-            if a[i] == minimo:
-                return i
-     else:
-        return -1
-    
 
 def minimo(a):
-    m = min(x for x in a if x>=0)
+    m = min(x.value for x in a if x.value >=0)
     return m
     
+def not_all_negative(numbers_prod):
+    result = False
+    for i in numbers_prod:
+        if i.value >= 0:
+            result = True
+    return result
+
+def producer(sem, number, full):
+    for i in range(N): #producimos N numeros
+        number.value += randint(1, 7)
+        print("PRODUCIENDO")
+        sem.release()
+        full.acquire()
     
-def consumer(numbers_prod, lista_sem, full):
-    index = index_min(numbers_prod)
-    lista_sem[index].release()
-    number = minimo(numbers_prod)
-    full.acquire()
-    pass
+    number.value = -1
+    sem.release()
+    
+
+def consumer(numbers_prod, lista_sem, lista_full, result):
+    
+    #Primero esperamos a que todos lor productores produzcan
+    for s in lista_sem:
+        s.acquire()
+    
+    indice = 0
+    
+    while not_all_negative(numbers_prod):
+        index = index_min(numbers_prod)
+        number = minimo(numbers_prod)
+        print("cogiendo el un numero")
+        lista_full[index].release()
+        delay()
+        lista_sem[index].acquire()
+        result[indice] = number
+        
+        indice +=1
+        print("escribiendo el numero")
+    
+    
     
 def main():
-    pass
+    
+    res = Array('i',NPROD * N)
+    numbers_prod = [Value('i',0) for i in range(NPROD)]
+    
+    lista_sem = [Semaphore(0) for i in range(NPROD)]
+    lista_full = [Semaphore(0) for i in range(NPROD)]
+    
+    lista_prod = [Process(target=producer,
+                        name=f'prod_{i}',
+                        args=(lista_sem[i], numbers_prod[i], lista_full[i]))
+                        for i in range(NPROD)]
+    
+    cons = Process(target=consumer,
+                        name='cons',
+                        args=(numbers_prod, lista_sem, lista_full, res))
+    
+    #Hacemos que los productores produzcan por primera vez:
+        
+    for p in lista_prod:
+        
+        p.start()
+    
+    cons.start()
+    
+    
+    for p in lista_prod:
+        
+        p.join()
+    
+    cons.join()
+    
+    print(list(res))
+
+if __name__ == "__main__":
+    main()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     
     
