816551891750b5e2d396f2ec0797738b3a6e6054
PabloBejarano10 <manbejar@ucm.es>
2023-03-02 10:50:50 +0100

    Comentarios

--- Practica_1.py
+++ Practica_1.py
@@ -9,16 +9,12 @@ NPROD = 3
 N = 5 #cantidad de numeros generados por cada productor 
 
 def delay(factor = 3):
-    sleep(random()/factor)
-
-def add_number(storage, index, data, full):
-    full.acquire()
-    storage[index.value] = data
-    delay(6)
-    index.value = index.value + 1
-    
+    sleep(random()/factor)    
     
 def index_min(a):
+    """
+    Esta funcion devuelve el indice del array a donde se encuentra el minimo
+    """
     
     l = []
     for x in a:
@@ -30,10 +26,17 @@ def index_min(a):
 
 
 def minimo(a):
+    """
+    Devuelve el minimo del array a 
+    """
     m = min(x.value for x in a if x.value >=0)
     return m
     
 def not_all_negative(numbers_prod):
+    """
+    Devuelve True si no todos los elementos son negativos y False si hay alguno
+    que sea positivo o cero
+    """
     result = False
     for i in numbers_prod:
         if i.value >= 0:
@@ -41,6 +44,14 @@ def not_all_negative(numbers_prod):
     return result
 
 def producer(sem, number, full):
+    """
+    PRODUCTOR:
+        Cada productor produce N numeros y el N+1 sera -1. 
+        Para evitar la exclusion mutua, debemos tener en cuenta que hay que hacer
+        signal cuando el productor haya terminado y wait del semaforo quye indica si 
+        la lista donde guardamos los productos esté llena
+    """
+    
     for i in range(N): #producimos N numeros
         number.value += randint(1, 7)
         print("PRODUCIENDO")
@@ -52,7 +63,12 @@ def producer(sem, number, full):
     
 
 def consumer(numbers_prod, lista_sem, lista_full, result):
-    
+    """
+    CONSUMIDOR:
+        Espera a que todos los productores hayan producido la primera vez, 
+        y posteriormente hace signal en el semaforo del productor del que ha consumido 
+        y pone a wait el semaforo de estar lleno donde se guardan los numeros producidos
+    """
     #Primero esperamos a que todos lor productores produzcan
     for s in lista_sem:
         s.acquire()
@@ -74,7 +90,16 @@ def consumer(numbers_prod, lista_sem, lista_full, result):
     
     
 def main():
+    """
+    res es el arry donde se guardan los numeros ordenados
+    numbers_prod es una lista con los numeros(en Values) producidos
     
+    lista_sem son los semaforos que esperaran a que los productores produzcan
+    lista_full son los semaforos que revisaran que el productor i ha producido
+    
+    lista_prod es la lista con los productores
+    cons es el consumidor
+    """
     res = Array('i',NPROD * N)
     numbers_prod = [Value('i',0) for i in range(NPROD)]
     
@@ -105,7 +130,7 @@ def main():
     
     cons.join()
     
-    print(list(res))
+    print(f"La lista final es: {list(res)}")
 
 if __name__ == "__main__":
     main()
